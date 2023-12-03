838f46609c8aa278c6b84f35ce92221996648873
VinzYK <152787135+VinzYK@users.noreply.github.com>
2023-12-03 19:49:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", ( ) => {
+    question.innerHTML = "Aaaaaa I Like Youuu Too"; gif.src = "https://raw.githubusercontent.com/DzareDeveloper/Img/main/gifyou.webp";
+});
+noBtn.addEventListener("mouseover", ( ) => {
+    const noBtnReact = noBtn.getBoundingClientRect( );
+    const maxX = window.innerWidth - noBtnReact.width;
+    const maxY = window.innerHeight - noBtnReact.height;
+
+    const randomX = Math.floor(Math.random( ) * maxX);
+    const randomY = Math.floor(Math.random( ) * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+});
